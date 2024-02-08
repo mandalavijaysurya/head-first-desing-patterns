eecad85784136eec5fc250e9b061f1d55a2dc410
Lynx <mandala.vijay.surya@gmail.com>
2024-02-09 01:22:58 +0530

    Added code to ForeCastDisplay display

--- src/main/java/org/headfirstdesignpattern/chapter2/weatherapplication/observers/displays/ForecastDisplay.java
+++ src/main/java/org/headfirstdesignpattern/chapter2/weatherapplication/observers/displays/ForecastDisplay.java
@@ -1,15 +1,28 @@
 package org.headfirstdesignpattern.chapter2.weatherapplication.observers.displays;
 
 import org.headfirstdesignpattern.chapter2.weatherapplication.observers.Observer;
+import org.headfirstdesignpattern.chapter2.weatherapplication.subjects.WeatherData;
 
 public class ForecastDisplay implements DisplayElement, Observer {
+
+    private WeatherData weatherData;
+    private float currentPressure = 29.2f;
+    private float lastPressure;
+
+    public ForecastDisplay(WeatherData weatherData){
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
     @Override
     public void update()  {
-
+        lastPressure = currentPressure;
+        currentPressure = weatherData.getPressure();
+        display();
     }
 
     @Override
     public void display() {
-
+        System.out.println("Current and Previous pressures are: " +currentPressure + ", " + lastPressure);
     }
 }
